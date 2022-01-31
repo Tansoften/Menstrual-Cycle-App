b30c209e0637b6666e23910fc8ffc50968a1b92b
Yona Godwin Yona <73188647+Yogona@users.noreply.github.com>
2022-01-31 10:13:46 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
-  static const myAppTitle = 'Period Calendar';
+  static const myAppTitle = 'Digital Cal';
 
   @override
   Widget build(BuildContext context) {
